659317eefa6907bf67e0a165f713743ecbd8a9ce
Daniel Velasquez <dvn228@nyu.edu>
2021-12-11 13:43:01 -0500

    added getall endpoint and function to reviews_router and review_repo respectively

--- routing/reviews_router.go
+++ routing/reviews_router.go
@@ -4,12 +4,14 @@ import (
 	"github.com/dannyvelas/go-backend/routing/internal"
 	"github.com/dannyvelas/go-backend/storage"
 	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog/log"
 	"net/http"
 )
 
 func ReviewsRouter(reviewRepo storage.ReviewRepo) func(chi.Router) {
 	return func(r chi.Router) {
 		r.Get("/active", GetActive(reviewRepo))
+		r.Get("/all", GetAll(reviewRepo))
 	}
 }
 
@@ -26,5 +28,23 @@ func GetActive(reviewRepo storage.ReviewRepo) http.HandlerFunc {
 		}
 
 		internal.RespondJson(w, http.StatusOK, activeReviews)
+	}
+}
+
+func GetAll(reviewRepo storage.ReviewRepo) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Info().Msg("Get All Endpoint")
+
+		page := internal.ToUint(r.URL.Query().Get("page"))
+		size := internal.ToUint(r.URL.Query().Get("size"))
+		limit, offset := internal.PagingToLimitOffset(page, size)
+
+		allReviews, err := reviewRepo.GetAll(limit, offset)
+		if err != nil {
+			internal.HandleInternalError(w, "Error querying reviewRepo: "+err.Error())
+			return
+		}
+
+		internal.RespondJson(w, http.StatusOK, allReviews)
 	}
 }
--- storage/review_repo.go
+++ storage/review_repo.go
@@ -65,3 +65,54 @@ func (reviewRepo *ReviewRepo) GetActive(limit, offset int) ([]models.Review, err
 
 	return reviews, nil
 }
+
+func (reviewRepo *ReviewRepo) GetAll(limit, offset int) ([]models.Review, error) {
+	const query = `
+    SELECT
+      reviews.id,
+      reviews.user_id,
+      reviews.book_id,
+      books.title_and_author,
+      reviews.review_date,
+      reviews.amt_stars,
+      reviews.description,
+      reviews.is_anonymous
+    FROM reviews
+    LEFT JOIN books ON
+      reviews.book_id = books.book_id 
+    LIMIT $1
+    OFFSET $2
+  `
+
+	rows, err := reviewRepo.database.driver.Query(query, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var reviews []models.Review
+	for rows.Next() {
+		var review models.Review
+		err := rows.Scan(
+			&review.Id,
+			&review.UserId,
+			&review.BookId,
+			&review.TitleAndAuthor,
+			&review.ReviewDate,
+			&review.AmtStars,
+			&review.Description,
+			&review.IsAnonymous,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		reviews = append(reviews, review)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return reviews, nil
+}
