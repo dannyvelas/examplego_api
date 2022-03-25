c6f767684c1ceba8bdc33e501ae158cecfe6ef38
Daniel Velasquez <danivelas4@gmail.com>
2022-03-25 19:47:22 -0400

    models: updated review model

--- models/review.go
+++ models/review.go
@@ -1,12 +1,10 @@
 package models
 
 type Review struct {
-	Id            int
-	UserId    string
-	BookId  string
-	TitleAndAuthor string
-	ReviewDate     int
-	AmtStars       int
-	Description   int
-	IsAnonymous   bool
+	Id          int
+	UserId      string
+	BookId      string
+	ReviewDate  int
+	AmtStars    int
+	Description int
 }
