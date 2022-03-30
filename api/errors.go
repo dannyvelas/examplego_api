bcf3a37cc00684796c0f553dc3c0d04d564fba2a
Daniel Velasquez <danivelas4@gmail.com>
2022-03-30 18:36:03 -0400

    api/errors: renamed `sentinel` struct to `sentinelError`

--- api/errors.go
+++ api/errors.go
@@ -8,22 +8,22 @@ type apiError interface {
 	apiError() (int, string)
 }
 
-type sentinel struct {
+type sentinelError struct {
 	statusCode int
 	message    string
 }
 
 var (
-	errUnauthorized        = sentinel{http.StatusUnauthorized, "Unauthorized"}
-	errBadRequest          = sentinel{http.StatusBadRequest, "Bad Request"}
-	errNotFound            = sentinel{http.StatusNotFound, "Not Found"}
-	errInternalServerError = sentinel{http.StatusInternalServerError, "Internal Server Error"}
+	errUnauthorized        = sentinelError{http.StatusUnauthorized, "Unauthorized"}
+	errBadRequest          = sentinelError{http.StatusBadRequest, "Bad Request"}
+	errNotFound            = sentinelError{http.StatusNotFound, "Not Found"}
+	errInternalServerError = sentinelError{http.StatusInternalServerError, "Internal Server Error"}
 )
 
-func (sentinel sentinel) Error() string {
-	return sentinel.message
+func (e sentinelError) Error() string {
+	return e.message
 }
 
-func (sentinel sentinel) apiError() (int, string) {
-	return sentinel.statusCode, sentinel.message
+func (e sentinelError) apiError() (int, string) {
+	return e.statusCode, e.message
 }
