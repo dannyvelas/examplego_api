d1bdcb19fd8706526a607b65a09b47fa0b5bf976
Daniel Velasquez <danivelas4@gmail.com>
2022-04-02 12:58:02 -0400

    models/review: changed reviewDate datatype to Time

--- models/review.go
+++ models/review.go
@@ -1,10 +1,14 @@
 package models
 
+import (
+	"time"
+)
+
 type Review struct {
 	Id          int
 	UserId      string
 	BookId      string
-	ReviewDate  int
+	ReviewDate  time.Time
 	AmtStars    int
 	Description int
 }
